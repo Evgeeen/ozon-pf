a68102d602dbbba721255ccaa38cfb339eef43d8
Evgeniy <evgeniyy.lapin@gmail.com>
2026-03-05 13:30:55 +0700

    Fix

--- src/CampaignClient.php
+++ src/CampaignClient.php
@@ -6,19 +6,18 @@ namespace Evgeeen;
 
 use Evgeeen\Models\Requests\GetCampaignRequest;
 use Evgeeen\Models\Requests\GetCampaignStatisticRequest;
-use Evgeeen\Models\Requests\GetReportsListRequest;
-use Evgeeen\Models\Responses\CheckReportResponse;
+use Evgeeen\Models\Responses\Report;
 use Evgeeen\Models\Responses\GetCampaignResponse;
 use Evgeeen\Models\Responses\GetCampaignStatisticResponse;
 use GuzzleHttp\RequestOptions;
 
 class CampaignClient extends Client
 {
-    private const GET_CAMPAIGN = "/api/client/campaign";
-    private const CREATE_STATISTIC = "/api/client/statistics";
-    private const CHECK_REPORT_STATUS = "/api/client/statistics/{uuid}";
-    private const GET_REPORT = "/api/client/statistics/report";
-    private const GET_REPORT_LIST = "/api/client/statistics/externallist";
+    private const GET_CAMPAIGN = "api/client/campaign";
+    private const CREATE_STATISTIC = "api/client/statistics/json";
+    private const CHECK_REPORT_STATUS = "api/client/statistics/{uuid}";
+    private const GET_REPORT = "api/client/statistics/report";
+    private const GET_REPORT_LIST = "api/client/statistics/externallist";
 
     public function get(GetCampaignRequest $request): GetCampaignResponse
     {
@@ -38,27 +37,22 @@ class CampaignClient extends Client
         return GetCampaignStatisticResponse::fromPrimitives($this->getDecodedBody($response->getBody()));
     }
 
-    public function checkReport(string $uuid): CheckReportResponse
+    public function checkReport(string $uuid): Report
     {
         $response = $this->sendRequest(
             "GET",
             $this->prepareUri(self::CHECK_REPORT_STATUS, ['uuid' => $uuid])
         );
 
-        return CheckReportResponse::fromPrimitives($this->getDecodedBody($response->getBody()));
+        return Report::fromPrimitives($this->getDecodedBody($response->getBody()));
     }
 
     public function getReport(string $uuid): string
     {
         $response = $this->sendRequest("GET", self::GET_REPORT, [
-            RequestOptions::QUERY => ['uuid' => $uuid],
+            RequestOptions::QUERY => ['UUID' => $uuid],
         ]);
 
         return $response->getBody()->getContents();
-    }
-
-    public function getReports(GetReportsListRequest $request): GetReportsListResponse
-    {
-
     }
 }
